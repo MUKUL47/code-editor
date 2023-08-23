8801284d0db78ff10cd27245042150e7278a5dcf
Mukul Dutt <mukuldutt1234@gmail.com>
2023-08-24 00:20:58 +0200

    update-row_text-cursor-on-keyup

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 let activeRowIndex = -1;
 let finalRowIndex = -1;
 const IDE = window["ide"];
+const TEXT_CURSOR = window["TEXT_CURSOR"];
 function span() {
   return document.createElement("span");
 }
 function newRow(n) {
   const r = document.createElement("div");
-  r.style.top = `${n * 20}px`;
+  r.style.top = `${n * 15}px`;
   r.setAttribute("row-index", n);
   r.className = "newline";
   return r;
@@ -19,39 +20,58 @@ function textCursor() {
   s.style.position = "absolute";
   return s;
 }
-document.body.appendChild;
-// IDE.appendChild(textCursor());
-// const TEXT_CURSOR = this["text-pipe"];
 function addNewLine() {
   IDE.appendChild(newRow(++activeRowIndex));
   finalRowIndex = activeRowIndex;
+  addNewTextSpan();
+}
+function addNewTextSpan() {
   const s = span();
   s.setAttribute("content", !!1);
-  IDE.children[activeRowIndex].appendChild(s);
+  getLineRow()?.appendChild(s);
+}
+function getLineRow() {
+  return document.querySelector(`div[row-index="${activeRowIndex}"]`);
 }
 addNewLine();
+updateTextCursor();
 IDE.addEventListener("click", (e) => {
   activeRowIndex = e.target.getAttribute("row-index") ?? finalRowIndex;
-  if (e.srcElement?.hasAttribute("content")) {
-    addTextCursor(e, e.srcElement);
-  }
+  updateTextCursor();
 });
+function getActiveRow() {
+  return document.querySelector(`div[row-index="${activeRowIndex}"]`);
+}
+function getLastRowOfLine() {
+  const row = getActiveRow();
+  return row.children[row.children.length - 1];
+}
 window.addEventListener("keydown", (event) => {
   const code = event.code.toLowerCase(); //Alt Space
-  if (code === "enter") return addNewLine();
+  if (code === "enter") {
+    if (activeRowIndex === finalRowIndex) {
+      addNewLine();
+    } else {
+      activeRowIndex++;
+    }
+    updateTextCursor();
+    return;
+  }
   const keyCode = event.keyCode || event.which;
   const isValidKey = keyCode >= 32 && keyCode <= 126;
   //get latest span and append the key;
   const row = document.querySelector(`div[row-index="${activeRowIndex}"]`); //IDE.children[activeRowIndex];
-  const lastSpan = row.children[row.children.length - 1];
+  const lastSpan = getLastRowOfLine();
+  let newRow;
   if (["space", "tab"].includes(code)) {
     //check if last span contains space or tab use that else append new
-    addNonBreakingSpace({ lastSpan, row });
-    moveCursor(3);
+    newRow = addNonBreakingSpace({ lastSpan, row });
   } else if (isValidKey) {
-    addText({ lastSpan, row, event });
-    moveCursor(3);
+    newRow = addText({ lastSpan, row, event });
+  } else if (code === "backspace") {
+    newRow = onBackspace({ lastSpan, row });
   }
+  updateTextCursor(newRow || lastSpan);
 });
 function addNonBreakingSpace({ lastSpan, row }) {
   if (lastSpan.innerHTML.includes("&nbsp;")) {
@@ -61,52 +81,48 @@ function addNonBreakingSpace({ lastSpan, row }) {
     s.setAttribute("content", !!1);
     s.innerHTML = "&nbsp";
     row.appendChild(s);
+    return s;
   }
 }
+
+function onBackspace({ lastSpan, row }) {
+  const len = lastSpan.innerHTML.length;
+  if (
+    row.children.length === 1 &&
+    !!!lastSpan.innerHTML.length &&
+    activeRowIndex > 0
+  ) {
+    activeRowIndex--;
+    return getLastRowOfLine();
+  }
+  const sliceIdx = wasSpaceLast(lastSpan) ? 6 : 1;
+  lastSpan.innerHTML = lastSpan.innerHTML.slice(0, len - sliceIdx);
+  if (!!!lastSpan.innerHTML.length && row.children.length > 1) {
+    lastSpan.remove();
+    return getLastRowOfLine();
+  }
+  return lastSpan;
+}
+function wasSpaceLast(span) {
+  return span.innerHTML.slice(span.innerHTML.length - 6) === "&nbsp;";
+}
 function addText({ lastSpan, row, event }) {
-  const wasSpace = lastSpan.innerHTML.slice(lastSpan.innerHTML.length - 6);
-  if (wasSpace === "&nbsp;") {
+  if (wasSpaceLast(lastSpan)) {
     const s = span();
     s.innerHTML = event.key;
     s.setAttribute("content", !!1);
     row.appendChild(s);
-    return;
+    return row;
   }
   lastSpan.innerHTML += event.key;
 }
-function addTextCursor(e, spanE) {
-  document.getElementById("text-pipe")?.remove();
-  const textCursor = span();
-  textCursor.className = "text-pipe";
-  textCursor.id = "text-pipe";
-  textCursor.textContent = "|";
-  textCursor.style.position = "absolute";
-  textCursor.style.left = `${e.clientX - spanE.getBoundingClientRect().left}px`;
-  textCursor.style.top = `${e.clientY - spanE.getBoundingClientRect().top}px`;
-  document.body.appendChild(textCursor);
+function updateTextCursor() {
+  TEXT_CURSOR.style.left = "0px"; //`${e.clientX - spanE.getBoundingClientRect().left}px`;
+  TEXT_CURSOR.style.top = `${activeRowIndex * 15}px`; //`${e.clientY - spanE.getBoundingClientRect().top}px`;
 }
-function moveCursor(len) {
-  const textCursor = document.getElementById("text-pipe");
-  if (!textCursor) return;
-  textCursor.style.left = `${
-    Number(textCursor.style.left.replace("px", "")) + len
-  }px`;
+function updateTextCursor() {
+  const spanTextElement = getLastRowOfLine();
+  TEXT_CURSOR.style.top = `${activeRowIndex * 15}px`;
+  TEXT_CURSOR.style.left =
+    spanTextElement.offsetLeft + spanTextElement.offsetWidth + "px";
 }
-
-const divElement = document.getElementById("ide");
-
-// Remove user-select:none when the mouse enters the div
-// divElement.addEventListener("mouseenter", function () {
-//   divElement.style.userSelect = "auto";
-// });
-
-// // Apply user-select:none again when the mouse leaves the div
-// divElement.addEventListener("mouseleave", function () {
-//   divElement.style.userSelect = "none";
-// });
-
-// Listen for the select event
-divElement.addEventListener("select", function (event) {
-  const selectedText = window.getSelection().toString();
-  console.log("Selected text:", selectedText);
-});
